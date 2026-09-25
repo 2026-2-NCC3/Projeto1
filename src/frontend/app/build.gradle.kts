7b788f11f6f9105379188de8225adfc6d2d97a4f
Guilherme Belcastro Medeiros <guilhermebelcastr12@gmail.com>
2026-09-25 19:27:16 -0300

    Atualização - Criação Telas de Login e Cadastro
    Atualizações internas no código e criação de 2 novas telas, sendo elas de login de usuários (já existentes) e cadastro de novos usuários.

--- src/frontend/app/build.gradle.kts
+++ src/frontend/app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.activity.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -41,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
